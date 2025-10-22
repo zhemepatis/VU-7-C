92be82d107f1f4b80e3c2b037d8e7c59379ebe6c
zhemepatis <rink.gab@gmail.com>
2025-10-22 19:40:50 +0300

    removed comments

--- benchmark_functions/sphere.py
+++ benchmark_functions/sphere.py
@@ -1,14 +1,5 @@
 import numpy as np
 
-# TODO:
-# [] choose domain
-# [] choose dimention num
-# [] generate inp vectors
-# [] should data be normalized?
-
-# [] apply neural network
-# [] get stats
-
 def sphere_func(vector):
     vector_squared = vector**2
     scalar = np.sum(vector_squared)
